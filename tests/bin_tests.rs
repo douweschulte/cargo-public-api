aba0e5fa7091c603323cd49b8f3f9d51a8d316b5
Martin Nordholts <enselic@gmail.com>
2022-04-02 17:27:33 +0200

    wip

--- tests/bin_tests.rs
+++ tests/bin_tests.rs
@@ -45,6 +45,35 @@ fn diff_public_items() {
         .success();
 }
 
+#[test]
+fn diff_public_items_color() {
+    ensure_test_crate_is_cloned();
+
+    let mut cmd = Command::cargo_bin("cargo-public-items").unwrap();
+    cmd.current_dir(test_crate_path());
+    cmd.arg("--color=always");
+    cmd.arg("--diff-git-checkouts");
+    cmd.arg("v0.0.4");
+    cmd.arg("v0.0.5");
+    cmd.assert()
+        .stdout(
+            "Removed items from the public API\n\
+             =================================\n\
+             -pub fn public_items::from_rustdoc_json_str(rustdoc_json_str: &str) -> Result<HashSet<String>>\n\
+             \n\
+             Changed items in the public API\n\
+             ===============================\n\
+             (none)\n\
+             \n\
+             Added items to the public API\n\
+             =============================\n\
+             +pub fn public_items::sorted_public_items_from_rustdoc_json_str(rustdoc_json_str: &str) -> Result<Vec<String>>\n\
+             \n\
+            ",
+        )
+        .success();
+}
+
 fn ensure_test_crate_is_cloned() {
     if !test_crate_path().exists() {
         clone_test_crate();
@@ -83,12 +112,15 @@ fn assert_presence_of_args_in_help(mut cmd: Command) {
         .success();
 }
 
+/// Helper to get the absolute path to a given path, relative to the current
+/// path
 fn current_dir_and<P: AsRef<Path>>(path: P) -> PathBuf {
     let mut cur_dir = std::env::current_dir().unwrap();
     cur_dir.push(path);
     cur_dir
 }
 
+/// Helper to clone the test crate git repo to the proper place
 fn clone_test_crate() {
     let mut git = std::process::Command::new("git");
     git.arg("clone");
@@ -97,8 +129,18 @@ fn clone_test_crate() {
     assert!(git.spawn().unwrap().wait().unwrap().success());
 }
 
+/// Path to the git cloned test crate we use to test the diffing functionality
 fn test_crate_path() -> PathBuf {
-    let mut path = PathBuf::from(option_env!("CARGO_TARGET_DIR").unwrap_or("/tmp"));
+    let mut path = get_cache_dir();
     path.push("cargo-public-items-test-repo");
     path
+}
+
+/// Where to put things that survives across tests runs. For example a git
+/// cloned test crate. We don't want to clone it every time we run tests. We
+/// want to clone it just once.
+fn get_cache_dir() -> PathBuf {
+    option_env!("CARGO_TARGET_DIR")
+        .map(|p| PathBuf::from(p))
+        .unwrap_or_else(|| std::env::temp_dir())
 }
